894a628e90d0b2738d2cb63805b1b4fc261b0127
Juliana Izmaylova <aeliren12@yandex.ru>
2024-06-10 13:36:53 +0300

    Full project is ready

---
+++ Coffee/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeOrdering
+{
+    public interface Coffee
+    {
+        string GetName();
+        double GetPrice();
+    }
+
+    public class Americano : Coffee
+    {
+        public string GetName()
+        {
+            return "Американо";
+        }
+
+        public double GetPrice()
+        {
+            return 100;
+        }
+    }
+
+    public class Cappuccino : Coffee
+    {
+        public string GetName()
+        {
+            return "Каппучино";
+        }
+
+        public double GetPrice()
+        {
+            return 150;
+        }
+    }
+
+    public class Latte : Coffee
+    {
+        public string GetName()
+        {
+            return "Латте";
+        }
+
+        public double GetPrice()
+        {
+            return 180;
+        }
+    }
+
+    public class CoffeeOrder
+    {
+        private List<Coffee> _coffees = new List<Coffee>();
+
+        public void AddCoffee(Coffee coffee)
+        {
+            _coffees.Add(coffee);
+        }
+
+        public double GetTotalPrice()
+        {
+            double totalPrice = 0;
+            foreach (var coffee in _coffees)
+            {
+                totalPrice += coffee.GetPrice();
+            }
+            return totalPrice;
+        }
+    }
+}
---
+++ Coffee/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections.Generic;
+
+namespace CoffeeOrdering
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private CoffeeOrder _order = new CoffeeOrder();
+        private Dictionary<string, int> _coffeeCounts = new Dictionary<string, int>();
+        private double totalPrice;
+        Coffee coffee = null;
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem selectedItem = (ListBoxItem)coffeeList.SelectedItem;
+            string coffeeName = selectedItem.Content.ToString();
+
+
+            switch (coffeeName)
+            {
+                case "Американо":
+                    coffee = new Americano();
+                    break;
+                case "Каппучино":
+                    coffee = new Cappuccino();
+                    break;
+                case "Латте":
+                    coffee = new Latte();
+                    break;
+            }
+
+            _order.AddCoffee(coffee);
+
+            // Get the existing count for the selected coffee
+            int count = 0;
+            if (_coffeeCounts.ContainsKey(coffeeName))
+            {
+                count = _coffeeCounts[coffeeName];
+            }
+
+            // Update the count
+            count++;
+            _coffeeCounts[coffeeName] = count;
+
+            UpdateOrderListBox();
+            UpdateTotalPrice();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            _coffeeCounts.Clear();
+            orderListBox.Items.Clear();
+            totalPrice = 0;
+            totalPriceLabel.Content = "";
+        }
+
+        private void UpdateOrderListBox()
+        {
+            orderListBox.Items.Clear();
+
+            foreach (var coffeeCount in _coffeeCounts)
+            {
+                orderListBox.Items.Add(new ListBoxItem()
+                {
+                    Content = $"{coffeeCount.Key} ({coffeeCount.Value})"
+                });
+            }
+        }
+
+        private void UpdateTotalPrice()
+        {
+            double totalPrice = _order.GetTotalPrice();
+            ListBoxItem selectedItem = (ListBoxItem)coffeeList.SelectedItem;
+            string coffeeName = selectedItem.Content.ToString();
+            totalPriceLabel.Content = $"Итого: {totalPrice} ₽";
+
+        }
+    }
+}
